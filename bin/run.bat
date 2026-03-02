fe315c4844c9ff60973aec8073492031014c32b5
N1etZsch3-Savior <3566624043@qq.com>
2026-03-02 13:38:33 +0800

    Init

--- bin/run.bat
+++ bin/run.bat
@@ -7,6 +7,7 @@ cd %~dp0
 cd ../ruoyi-admin/target
 
 set JAVA_OPTS=-Xms256m -Xmx1024m -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=512m
+if not defined LOG_PATH set LOG_PATH=%~dp0..\logs
 
 java -jar %JAVA_OPTS% ruoyi-admin.jar
 
